74bc11311951ec8971674840d433ca05a264c67c
EaglesBusinessGroup <44905832+EaglesBusinessGroup@users.noreply.github.com>
2018-11-09 12:37:45 -0600

    Add files via upload

---
+++ prgram1.py
@@ -0,0 +1,23 @@
+# We use '#' to make a single line comment
+'''
+we use '''    ''' to make a multi-line comment
+Author: Farris Hassan
+Comments are in computer programming,
+a comment is a programmer-readable explanation or annotation in the source code of a computer program.
+They are added with the purpose of making the source code easier for humans to understand,
+and are generally ignored by compilers and interpreters.
+'''
+#This program will print out the string "Welcome!" to the screen.
+print("Welcome!")
+
+#"\n" creates new blank line
+print("\n")
+
+#The following two lines declare two variables called num1 and num2
+#And initializes the variables with the value of 3 and 5 
+num1 = 3
+num2 = 5
+#The following line adds num1 and num2 and assigns the result to another variable num3 
+num3 = num1 + num2
+#The following line displays the result on the screen 
+print("The sum of ", num1, "and", num2, "is", num3)
